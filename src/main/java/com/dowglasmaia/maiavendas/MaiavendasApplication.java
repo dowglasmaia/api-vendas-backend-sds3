721c9e6fddd54fe4b10cec4c1d527cad1b1b9438
Dowglas Maia <dowglasmaia@gmail.com>
2021-09-07 09:37:34 -0300

    Criando projeto

---
+++ src/main/java/com/dowglasmaia/maiavendas/MaiavendasApplication.java
@@ -0,0 +1,13 @@
+package com.dowglasmaia.maiavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaiavendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaiavendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dowglasmaia/maiavendas/MaiavendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dowglasmaia.maiavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaiavendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
